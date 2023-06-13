e65c04a3e92ca7ab61f8974f094727ca63bec438
V3lasquez <noe.vc1782@gmail.com>
2023-06-13 14:19:35 -0500

    4

---
+++ src/main/java/com/tecsup/petclinic/entities/Vet.java
@@ -0,0 +1,43 @@
+package com.tecsup.petclinic.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+@Entity(name = "vets")
+@Data
+public class Vet {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Column(name = "first_name")
+	private String firstName;
+	@Column(name = "last_name")
+	private String lastName;
+
+	
+	public Vet() {
+	}
+
+	public Vet(Integer id, String first_name, String last_name) {
+		super();
+		this.id = id;
+		this.firstName = first_name;
+		this.lastName = last_name;
+
+	}
+
+	public Vet(String first_name, String last_name) {
+		super();
+		this.firstName = first_name;
+		this.lastName = last_name;
+	}
+}
