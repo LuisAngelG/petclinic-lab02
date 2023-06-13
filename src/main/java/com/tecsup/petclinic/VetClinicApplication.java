ca16ca5d14923424a0296713eba44ed9ec47b08e
AngeloR10 <angelorl102004@gmail.com>
2023-06-13 14:35:04 -0500

    5

---
+++ src/main/java/com/tecsup/petclinic/VetClinicApplication.java
@@ -0,0 +1,22 @@
+package com.tecsup.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author jgomezm
+ *
+ */
+@SpringBootApplication
+public class VetClinicApplication {
+
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(VetClinicApplication.class, args);
+	}
+
+}
