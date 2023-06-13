1f02241c3c319513a3cfbfa84fcc8d84e8b4964a
ANTONIOS-MAJ-HE <antonio.santiago@tecsup.edu.pe>
2023-06-13 15:06:54 -0500

    delete

--- src/test/java/com/tecsup/petclinic/services/VetServiceTest.java
+++ src/test/java/com/tecsup/petclinic/services/VetServiceTest.java
@@ -1,10 +1,52 @@
 package com.tecsup.petclinic.services;
 
 import lombok.extern.slf4j.Slf4j;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.tecsup.petclinic.entities.Vet;
+import com.tecsup.petclinic.exception.VetNotFoundException;
 
 @SpringBootTest
 @Slf4j
 public class VetServiceTest {
-	// TO DO
+	
+	
+    /**
+     * antonio
+    *
+    */
+   @Test
+   public void testDeleteVet() {
+
+       String VET_NAME = "Bird";
+
+       // ------------ Create ---------------
+
+       Vet vet = new Vet(VET_NAME);
+       vet = this.vetService.create(vet);
+       log.info("" + vet);
+
+       // ------------ Delete ---------------
+
+       try {
+           this.vetService.delete(vet.getId());
+       } catch (VetNotFoundException e) {
+           fail(e.getMessage());
+       }
+
+       // ------------ Validation ---------------
+
+       try {
+           this.vetService.findById(vet.getId());
+           assertTrue(false);
+       } catch (VetNotFoundException e) {
+           assertTrue(true);
+       }
+
+   }
 }
